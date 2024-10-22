002bfce05da2e2472521f46ccd560b553ff894d6
villrait <166745091+villrait@users.noreply.github.com>
2024-10-23 00:08:35 +0500

    3 commi. Add task N5

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//5 Объявить две опциональные переменные типа Double. Одной из них задать первоначальное значение. Написать функцию, которая принимает на вход опциональную переменную типа Double. Функция должна безопасно извлечь значение из входящей переменной. Если значение удалось получить - необходимо вывести его в консоль, если значение у переменной отсутствует вывести в консоль фразу "Variable can't be unwrapped". Вызвать функцию дважды с двумя ранее созданными переменными.
+//
+
+var doubleOptional: Double? = 2.2
+var doubleOptional2: Double?
+
+func printValueDouble(variable: Double?){
+    if let value = variable {
+        print(value)
+    } else {
+        print("Variable can't be unwrapped")
+    }
+}
+
+printValueDouble(variable: doubleOptional)
+printValueDouble(variable: doubleOptional2)
