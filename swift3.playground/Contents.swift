22451f97efda2b1d74b33c35208fb8faba462144
villrait <166745091+villrait@users.noreply.github.com>
2024-10-28 00:27:37 +0500

    8 commit. add task 8

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//8. Напишите программу, решающую задачу: есть входящая строка формата "abc123", где сначала идет любая последовательность букв, потом число. Необходимо получить новую строку, в конце которой будет число на единицу больше предыдущего, то есть "abc124".
+
+var text = "abc123"
+
+func addNumberToString(text: String) -> String {
+    var letters = ""
+    var number = 0
+    var resultText = ""
+    var currentNumber = ""
+    
+    for char in text {
+        if char.isNumber {
+            currentNumber += String(char)
+        } else {
+            letters += String(char)
+            if !currentNumber.isEmpty {
+                number += Int(currentNumber)!
+                currentNumber = ""
+            }
+        }
+    }
+    
+    if !currentNumber.isEmpty {
+        number += Int(currentNumber)!
+    }
+    
+    resultText = letters + String(number + 1)
+    
+    return resultText
+}
+
+var text2 = addNumberToString(text: text)
