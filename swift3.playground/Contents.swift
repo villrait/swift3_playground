7d3d2bd0abb249a310b4903538d0b001f419df7b
villrait <166745091+villrait@users.noreply.github.com>
2024-10-30 01:21:39 +0500

    11 commit. add task N11

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//11. Создать класс Rectangle с двумя неопциональными свойствами: ширина и длина. Реализовать в этом классе метод вычисляющий и выводящий в консоль периметр прямоугольника. Создать экземпляр класса и вызвать у него этот метод.
+
+class Rectangle {
+    var width: Int
+    var length: Int
+    
+    init(width: Int, length: Int) {
+        self.width = width
+        self.length = length
+    }
+    
+    func perimeterCalc() {
+        var p = 0
+        p = (width + length) * 2
+        print("reimetr = \(p)")
+    }
+}
+
+var rectangle = Rectangle(width: 5, length: 10)
+
+rectangle.perimeterCalc()
