2813837544910866c73a8e6adf6f1de4350f382d
villrait <166745091+villrait@users.noreply.github.com>
2024-11-09 16:50:44 +0500

    add fixCommit feature 1.0

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -3,34 +3,42 @@ import UIKit
 //5 Объявить две опциональные переменные типа Double. Одной из них задать первоначальное значение. Написать функцию, которая принимает на вход опциональную переменную типа Double. Функция должна безопасно извлечь значение из входящей переменной. Если значение удалось получить - необходимо вывести его в консоль, если значение у переменной отсутствует вывести в консоль фразу "Variable can't be unwrapped". Вызвать функцию дважды с двумя ранее созданными переменными.
 //
 
-var doubleOptional: Double? = 2.2
-var doubleOptional2: Double?
-
-func printValueDouble(variable: Double?){
-    if let value = variable {
-        print(value)
-    } else {
-        print("Variable can't be unwrapped")
+class TestTask {
+    var doubleOptional: Double? = 2.2
+    var doubleOptional2: Double?
+    
+    func printValueDouble(_ value: Double?) {
+        if let val = value {
+            print(val)
+        } else {
+            print("Variable can't be unwrapped")
+        }
+    }
+    
+    func runTest() {
+        printValueDouble(doubleOptional)
+        printValueDouble(doubleOptional2)
     }
 }
 
-printValueDouble(variable: doubleOptional)
-printValueDouble(variable: doubleOptional2)
+let test1 = TestTask()
+test1.runTest()
 
 
 //6. Напишите программу для вывода первых 15 чисел последовательности Фибоначчи
 
-func fibanachi(_ int: Int) -> Int {
-    if int == 0 {
-        return 0
-    } else if int == 1 {
-        return 1
+func calculateFibonachi(_ int: Int) -> Int {
+    if int <= 1 {
+        return int
     } else {
-        return fibanachi(int - 1) + fibanachi(int - 2)
+        return calculateFibonachi(int - 1) + calculateFibonachi(int - 2)
     }
 }
 
-fibanachi(6)
+for i in 0...15 {
+    print("calculateFibonachi: number \(i) = \(calculateFibonachi(i))")
+
+}
 
 
 //7. Напишите программу для сортировки массива, использующую метод пузырька. Сортировка должна происходить в отдельной функции, принимающей на вход исходный массив.
