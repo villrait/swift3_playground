2363a077a59f5bfe9ba176b5c8a9763a223eb38d
villrait <166745091+villrait@users.noreply.github.com>
2024-11-20 16:28:43 +0500

    add commit Feature 2.0

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -1,83 +1,101 @@
 import UIKit
 
-print("Task N8")
+protocol Testable {
+    func runTest()
+}
+
 //8. Напишите программу, решающую задачу: есть входящая строка формата "abc123", где сначала идет любая последовательность букв, потом число. Необходимо получить новую строку, в конце которой будет число на единицу больше предыдущего, то есть "abc124".
 
-func incrementNumber(_ string: String) -> String {
-    var result = string
-    var lastNumberRange: Range<String.Index>?
-    
-    for number in string.components(separatedBy: CharacterSet.decimalDigits.inverted) {
-        if let num = Int(number) {
-            lastNumberRange = string.range(of: number)
+class IncrementNumber: Testable {
+    private func incrementNumber(_ string: String) -> String {
+        var result = string
+        var lastNumberRange: Range<String.Index>?
+        
+        for number in string.components(separatedBy: CharacterSet.decimalDigits.inverted) {
+            if let num = Int(number) {
+                lastNumberRange = string.range(of: number)
+            }
         }
-    }
-    
-    if let range = lastNumberRange{
-        if let num = Int(string[range]) {
-            let incrementNumber = num + 1
-            result.replaceSubrange(range, with: String(incrementNumber))
+        
+        if let range = lastNumberRange{
+            if let num = Int(string[range]) {
+                let incrementNumber = num + 1
+                result.replaceSubrange(range, with: String(incrementNumber))
+            }
         }
+        
+        return result
     }
-    
-    return result
+
+    func runTest() {
+        var text1 = "abc123"
+        var text2 = "abc123de"
+        
+        var incrementText1 = incrementNumber(text1)
+        var incrementText2 = incrementNumber(text2)
+        
+        print("Increment text: \(text1) -> \(incrementText1)")
+        print("Increment text: \(text2) -> \(incrementText2)")
+    }
 }
 
-var text1 = "abc123"
-var text2 = "abc123de"
-
-var increment1 = incrementNumber(text1)
-var increment2 = incrementNumber(text2)
-
-print("increment abc123 = \(increment1)")
-print("increment abc123de = \(increment2)")
-
-
-print("\nTask N9")
 //9. Написать простое замыкание в переменной myClosure, замыкание должно выводить в консоль фразу "I love Swift". Вызвать это замыкание. Далее написать функцию, которая будет запускать заданное замыкание заданное количество раз. Объявить функцию так: func repeatTask (times: Int, task: () -> Void). Функция должна запускать times раз замыкание task . Используйте эту функцию для печати «I love Swift» 10 раз.
 
-var myClosure = {
-    print("I love Swift")
-}
+class MyClosure: Testable {
+    var myClosure = {
+        print("I love Swift")
+    }
 
-myClosure()
+    func repeatTask(times: Int, task: () -> Void) {
+        for i in 0..<times {
+            task()
+        }
+    }
 
-func repeatTask(times: Int, task: () -> Void) {
-    for i in 0..<times {
-        task()
+    func runTest() {
+        myClosure()
+        repeatTask(times: 10, task: myClosure)
     }
 }
-
-repeatTask(times: 10, task: myClosure)
 
 
 //10. Условия: есть начальная позиция на двумерной плоскости, можно осуществлять последовательность шагов по четырем направлениям up, down, left, right. Размерность каждого шага равна 1. Создать перечисление Directions с направлениями движения. Создать переменную location с начальными координатами (0,0), создать массив элементами которого будут направления из перечисления. Положить в этот массив следующую последовательность шагов: [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]. Програмно вычислить какие будут координаты у переменной location после выполнения этой последовательности шагов.
 
+class Location: Testable {
+    enum Directions {
+        case up
+        case left
+        case right
+        case down
+    }
 
-print("\nTask N10")
-enum Directions {
-    case up
-    case left
-    case right
-    case down
-}
+    var location = (x: 0, y: 0)
 
-var location = (x: 0, y: 0)
+    var steps: [Directions] = [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]
 
-var steps: [Directions] = [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]
+    func processSteps() {
+        for step in steps {
+            switch step {
+            case .right:
+                location.x += 1
+            case .left:
+                location.x -= 1
+            case .up:
+                location.y += 1
+            case .down:
+                location.y -= 1
+            }
+        }
+    }
 
-for step in steps {
-    switch step {
-    case .right:
-        location.x += 1
-    case .left:
-        location.x -= 1
-    case .up:
-        location.y += 1
-    case .down:
-        location.y -= 1
+    func runTest() {
+        print(location)
     }
 }
 
-print(location)
+let tasks: [Testable] = [IncrementNumber(), MyClosure(), Location()]
 
+for task in tasks {
+    print("\n")
+    task.runTest()
+}
