a1ba55a707e7a8bc69b7bb0a2aba8ac5be24d9dd
villrait <166745091+villrait@users.noreply.github.com>
2024-11-20 16:09:35 +0500

    add commit Feature 1.0

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -1,6 +1,5 @@
 import UIKit
 
-print("Task N5")
 //5 Объявить две опциональные переменные типа Double. Одной из них задать первоначальное значение. Написать функцию, которая принимает на вход опциональную переменную типа Double. Функция должна безопасно извлечь значение из входящей переменной. Если значение удалось получить - необходимо вывести его в консоль, если значение у переменной отсутствует вывести в консоль фразу "Variable can't be unwrapped". Вызвать функцию дважды с двумя ранее созданными переменными.
 //
 
@@ -26,23 +25,10 @@ class TestTask: Testable {
     }
 }
 
-class TestTask2: Testable {
-    func runTest() {
-        print("testing system ok")
-    }
-}
-let tasks: [Testable] = [TestTask(), TestTask2()] // если я правильно понял, то можно было еще пару классов написать с функцией runTest и добавить их в этот массив и они бы выполнили каждый свой код (к примеру разные ПРИНТЫ вывели бы)
-
-for task in tasks {
-    task.runTest()
-}
-
-
-print("\nTask N6")
 //6. Напишите программу для вывода первых 15 чисел последовательности Фибоначчи
 
-class Fibonachi {
-    func calculateFibonachi(_ int: Int) -> Int {
+class Fibonachi: Testable {
+    private func calculateFibonachi(_ int: Int) -> Int {
         if int <= 1 {
             return int
         } else {
@@ -50,7 +36,7 @@ class Fibonachi {
         }
     }
     
-    func printFibonachi() {
+    func runTest() {
         for i in 0...15 {
             print("calculateFibonachi: number \(i) = \(calculateFibonachi(i))")
             
@@ -58,14 +44,9 @@ class Fibonachi {
     }
 }
 
-let fibonacji = Fibonachi()
-fibonacji.printFibonachi()
-
-
-print("\nTask N7")
 //7. Напишите программу для сортировки массива, использующую метод пузырька. Сортировка должна происходить в отдельной функции, принимающей на вход исходный массив.
 
-class Bubble { 
+class Bubble: Testable {
     
     private func bubbleSort(array: [Int]) -> [Int] {
         var arr = array
@@ -82,17 +63,22 @@ class Bubble {
         return arr
     }
     
-    func bubbleShow(array: [Int]) {
-        print("Bubble sort: \(bubbleSort(array: array))")
+    func runTest() {
+        let unsortedArr = [3,2,5,1,8,0]
+        let sortedArr = bubbleSort(array: unsortedArr)
+        print("Bubble Sort: \(unsortedArr) -> \(sortedArr)")
     }
 }
-
-let bubble = Bubble()
-bubble.bubbleShow(array: [3,2,5,1,8,0])
-
-
 
 // ну или такой вариант мне более привычен и понятен(потому что чем больше в дебри логики кода начинаешь уходить, тем меньше я его понимаю)
 //
 //let bubble2 = [5,4,3,2,1]
 //bubble2.sorted { $0 < $1 }
+
+
+let tasks: [Testable] = [TestTask(), Fibonachi(), Bubble()]
+
+for task in tasks {
+    print("\n")
+    task.runTest()
+}
