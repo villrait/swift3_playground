f26331891734dd8e9ca12e713f7b7f49a0430fb1
villrait <166745091+villrait@users.noreply.github.com>
2024-11-25 23:21:38 +0500

    add commit feature 2.0

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -6,42 +6,37 @@ protocol Testable {
 
 //8. Напишите программу, решающую задачу: есть входящая строка формата "abc123", где сначала идет любая последовательность букв, потом число. Необходимо получить новую строку, в конце которой будет число на единицу больше предыдущего, то есть "abc124".
 
-class IncrementNumber: Testable {
+class IncrementNumberTask: Testable {
     private func incrementNumber(_ string: String) -> String {
+
         var result = string
-        var lastNumberRange: Range<String.Index>?
         
-        for number in string.components(separatedBy: CharacterSet.decimalDigits.inverted) {
-            if let num = Int(number) {
-                lastNumberRange = string.range(of: number)
-            }
+        guard let lastNumberSubstring = string.components(separatedBy: CharacterSet.decimalDigits.inverted).last,
+              let number = Int(lastNumberSubstring),
+              let lastNumberRange = string.range(of: lastNumberSubstring) else {
+            return string
         }
+
+        let incrementNumber = number + 1
         
-        if let range = lastNumberRange{
-            if let num = Int(string[range]) {
-                let incrementNumber = num + 1
-                result.replaceSubrange(range, with: String(incrementNumber))
-            }
-        }
-        
+        result.replaceSubrange(lastNumberRange, with: "\(incrementNumber)")
+
         return result
+
     }
 
     func runTest() {
         var text1 = "abc123"
-        var text2 = "abc123de"
         
         var incrementText1 = incrementNumber(text1)
-        var incrementText2 = incrementNumber(text2)
         
         print("Increment text: \(text1) -> \(incrementText1)")
-        print("Increment text: \(text2) -> \(incrementText2)")
     }
 }
 
 //9. Написать простое замыкание в переменной myClosure, замыкание должно выводить в консоль фразу "I love Swift". Вызвать это замыкание. Далее написать функцию, которая будет запускать заданное замыкание заданное количество раз. Объявить функцию так: func repeatTask (times: Int, task: () -> Void). Функция должна запускать times раз замыкание task . Используйте эту функцию для печати «I love Swift» 10 раз.
 
-class MyClosure: Testable {
+class ClosureTask: Testable {
     var myClosure = {
         print("I love Swift")
     }
@@ -61,7 +56,7 @@ class MyClosure: Testable {
 
 //10. Условия: есть начальная позиция на двумерной плоскости, можно осуществлять последовательность шагов по четырем направлениям up, down, left, right. Размерность каждого шага равна 1. Создать перечисление Directions с направлениями движения. Создать переменную location с начальными координатами (0,0), создать массив элементами которого будут направления из перечисления. Положить в этот массив следующую последовательность шагов: [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]. Програмно вычислить какие будут координаты у переменной location после выполнения этой последовательности шагов.
 
-class Location: Testable {
+class LocationTask: Testable {
     enum Directions {
         case up
         case left
@@ -93,7 +88,7 @@ class Location: Testable {
     }
 }
 
-let tasks: [Testable] = [IncrementNumber(), MyClosure(), Location()]
+let tasks: [Testable] = [IncrementNumberTask(), ClosureTask(), LocationTask()]
 
 for task in tasks {
     print("\n")
