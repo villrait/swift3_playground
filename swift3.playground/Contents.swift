44da87418b703819ee285d021b29482c4b16c220
villrait <166745091+villrait@users.noreply.github.com>
2024-12-05 00:49:05 +0500

    no message

--- swift3.playground/Contents.swift
+++ swift3.playground/Contents.swift
@@ -7,92 +7,76 @@ protocol Testable {
     func runTest()
 }
 
-//8. Напишите программу, решающую задачу: есть входящая строка формата "abc123", где сначала идет любая последовательность букв, потом число. Необходимо получить новую строку, в конце которой будет число на единицу больше предыдущего, то есть "abc124".
-
-class IncrementNumberTask: Testable {
-    private func incrementNumber(_ string: String) -> String {
-
-        var result = string
-        
-        guard let lastNumberSubstring = string.components(separatedBy: CharacterSet.decimalDigits.inverted).last,
-              let number = Int(lastNumberSubstring),
-              let lastNumberRange = string.range(of: lastNumberSubstring) else {
-            return string
+class TestTask: Testable {
+    private func show(_ value: Double?) { // имя функции show потому что при слове print - код ломается. В ифах ошибки возникают
+        if let val = value {
+            print(val)
+        } else {
+            print("Variable can't be unwrapped")
         }
-
-        let incrementNumber = number + 1
-        
-        result.replaceSubrange(lastNumberRange, with: "\(incrementNumber)")
-
-        return result
-
     }
-
+    
     func runTest() {
-        var text1 = "abc123"
+        var doubleOptional: Double? = 2.2
+        var doubleOptional2: Double?
         
-        var incrementText1 = incrementNumber(text1)
-        
-        print("Increment text: \(text1) -> \(incrementText1)")
+        show(doubleOptional)
+        show(doubleOptional2)
     }
 }
 
-//9. Написать простое замыкание в переменной myClosure, замыкание должно выводить в консоль фразу "I love Swift". Вызвать это замыкание. Далее написать функцию, которая будет запускать заданное замыкание заданное количество раз. Объявить функцию так: func repeatTask (times: Int, task: () -> Void). Функция должна запускать times раз замыкание task . Используйте эту функцию для печати «I love Swift» 10 раз.
+//6. Напишите программу для вывода первых 15 чисел последовательности Фибоначчи
 
-class ClosureTask: Testable {
-    var myClosure = {
-        print("I love Swift")
-    }
-
-    func repeatTask(times: Int, task: () -> Void) {
-        for i in 0..<times {
-            task()
+class Fibonachi: Testable {
+    private func calculateFibonachi(_ int: Int) -> Int {
+        if int <= 1 {
+            return int
+        } else {
+            return calculateFibonachi(int - 1) + calculateFibonachi(int - 2)
         }
     }
-
+    
     func runTest() {
-        myClosure()
-        repeatTask(times: 10, task: myClosure)
+        for i in 0...15 {
+            print("calculateFibonachi: number \(i) = \(calculateFibonachi(i))")
+            
+        }
     }
 }
 
+//7. Напишите программу для сортировки массива, использующую метод пузырька. Сортировка должна происходить в отдельной функции, принимающей на вход исходный массив.
 
-//10. Условия: есть начальная позиция на двумерной плоскости, можно осуществлять последовательность шагов по четырем направлениям up, down, left, right. Размерность каждого шага равна 1. Создать перечисление Directions с направлениями движения. Создать переменную location с начальными координатами (0,0), создать массив элементами которого будут направления из перечисления. Положить в этот массив следующую последовательность шагов: [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]. Програмно вычислить какие будут координаты у переменной location после выполнения этой последовательности шагов.
-
-class LocationTask: Testable {
-    enum Directions {
-        case up
-        case left
-        case right
-        case down
-    }
-
-    var location = (x: 0, y: 0)
-
-    var steps: [Directions] = [.up, .up, .left, .down, .left, .down, .down, .right, .right, .down, .right]
-
-    func processSteps() {
-        for step in steps {
-            switch step {
-            case .right:
-                location.x += 1
-            case .left:
-                location.x -= 1
-            case .up:
-                location.y += 1
-            case .down:
-                location.y -= 1
+class Bubble: Testable {
+    
+    private func bubbleSort(array: [Int]) -> [Int] {
+        var arr = array
+        var n = arr.count
+        for i in 0..<n-1 {
+            for j in 0..<n-i-1 {
+                if arr[j] > arr[j+1] {
+                    let temp = arr [j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+                }
             }
         }
         return arr
     }
-
+    
     func runTest() {
-        print(location)
+        let unsortedArr = [3,2,5,1,8,0]
+        let sortedArr = bubbleSort(array: unsortedArr)
+        print("Bubble Sort: \(unsortedArr) -> \(sortedArr)")
     }
 }
 
-let tasks: [Testable] = [IncrementNumberTask(), ClosureTask(), LocationTask()]
+// ну или такой вариант мне более привычен и понятен(потому что чем больше в дебри логики кода начинаешь уходить, тем меньше я его понимаю)
+//
+//let bubble2 = [5,4,3,2,1]
+//bubble2.sorted { $0 < $1 }
+
+
+let tasks: [Testable] = [TestTask(), Fibonachi(), Bubble()]
 
 for task in tasks {
     print("\n")
